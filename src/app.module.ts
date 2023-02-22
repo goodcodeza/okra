bde9c019f43b4f44c436ffe58b086d5c3641e28d
Nolan Pather <goodcodeza@gmail.com>
2023-02-22 11:50:53 +0200

    generate customers resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
